2830a376d1aad1cb650cd220ab8042e58a67b590
Ramin Mammadzada <remki-design@hotmail.com>
2021-04-06 09:44:12 +0300

    Add routes and navbar.

---
+++ src/Routes.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Missions from './components/Missions';
+import About from './components/About';
+
+const Routes = () => (
+  <BrowserRouter>
+    <Navbar />
+    <Switch>
+      <Route path="/about" component={About} />
+      <Route path="/missions" component={Missions} />
+      <Route path="/" component={Missions} />
+    </Switch>
+  </BrowserRouter>
+);
+
+export default Routes;
