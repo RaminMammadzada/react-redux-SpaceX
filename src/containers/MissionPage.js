0746fa9124bbd87831a1091dfb1284811a615c7a
Ramin Mammadzada <remki-design@hotmail.com>
2021-04-07 15:53:18 +0300

    Add mission video to the mission page.

--- src/containers/MissionPage.js
+++ src/containers/MissionPage.js
@@ -1,7 +1,9 @@
 import React, { useLayoutEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
+import ReactPlayer from 'react-player';
 import { fetchCurrentMission } from '../actions';
+import './MissionPage.css';
 
 const MissionPage = () => {
   const { id } = useParams();
@@ -13,14 +15,29 @@ const MissionPage = () => {
     dispatch(fetchCurrentMission(url));
   }, []);
 
-  return (
-    <div>
-      <p>{id}</p>
-      <h1>{currentMission.mission_name}</h1>
-      <h1>{currentMission.details}</h1>
-      <h1>{currentMission.launch_year}</h1>
-    </div>
-  );
+  if (currentMission.links !== undefined) {
+    return (
+      <div className="single-mission-page">
+        <div className="mission-details">
+          <ul>
+            <li>{`Flight no: ${id}`}</li>
+            <li>{`Mission name: ${currentMission.mission_name}`}</li>
+            <li>{`About launch: ${currentMission.details}`}</li>
+            <li>{`Launch year no: ${currentMission.launch_year}`}</li>
+            <li>{`Launch site: ${currentMission.launch_site.site_name}`}</li>
+          </ul>
+        </div>
+        <div className="mission-video">
+          <ReactPlayer
+            width="420px"
+            height="250px"
+            url={currentMission.links.video_link}
+          />
+        </div>
+      </div>
+    );
+  }
+  return (<div>Loading</div>);
 };
 
 export default MissionPage;
